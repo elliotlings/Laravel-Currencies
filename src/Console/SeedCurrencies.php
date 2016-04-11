cd2153d0f27f8180c6fc3bdd5867d58017177613
DraperStudio <hello@draperstudio.tech>
2016-04-11 15:07:52 +0200

    Update SeedCurrencies.php

--- src/Console/SeedCurrencies.php
+++ src/Console/SeedCurrencies.php
@@ -43,6 +43,6 @@ class SeedCurrencies extends Command
             Currency::create($country);
         }
 
-        $this->info('Currencies seeded!');
+        $this->getOutput()->writeln("<info>Seeded:</info> Currencies");
     }
 }
